e0c3c92931387dba3550d24cb008b3194c770199
johny anastacio <johnyanastaciods@gmail.com>
2020-09-29 18:36:42 -0300

    changes in bot

--- app.py
+++ app.py
@@ -20,24 +20,27 @@ class Instabot:
         time.sleep(3)
         self.driver.find_element_by_xpath("//button[contains(text(), 'Agora não')]")\
             .click()
-            
-    def get_unfollowers(self):
-        self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username))\
-            .click()
-        time.sleep(2)
-        self.driver.find_element_by_xpath("//a[contains(@href,'/following')]")\
-            .click()
-        time.sleep(2)
+    
+    def search_hastags(self, hastag):
+        self.driver.find_element_by_xpath("//input[@type=\"text\"]")\
+            .send_keys(hastag)
+   # def get_unfollowers(self):
+       # self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username))\
+        #    .click()
+        #time.sleep(2)
+        #self.driver.find_element_by_xpath("//a[contains(@href,'/following')]")\
+         #   .click()
+        #time.sleep(2)
         #sugs = self.driver.find_element_by_xpath("//h4[contains(text(), sugestões)]")
         #self.driver.execute_script("arguments[0].scrollIntoView")
         #time.sleep(2)
-        scroll_box =  self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]")
-        last_ht, ht = 0,1
-        time.sleep(1)
-        ht = self.driver.execute_script("""arguments[0].scrollTo(0, arguments[0].scrollHeight);
-        return arguments[0].scrollHeight;
-        """, scroll_box)
+        #scroll_box =  self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]")
+        #last_ht, ht = 0,1
+        #time.sleep(1)
+        #ht = self.driver.execute_script("""arguments[0].scrollTo(0, arguments[0].scrollHeight);
+        #return arguments[0].scrollHeight;
+        #""", scroll_box)
 
 
 my_bot = Instabot('johnyanastacio','johny@257310')
-my_bot.get_unfollowers()
+my_bot.search_hastags("#python")
